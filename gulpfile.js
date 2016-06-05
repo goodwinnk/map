f9ffc2c9f510695a020341abeb2dc6e23b763b5a
Nikolay Krasko <goodwinnk@gmail.com>
2016-06-06 00:16:46 +0300

    Add empty style file

--- gulpfile.js
+++ gulpfile.js
@@ -6,5 +6,5 @@ gulp.task('default', function() {
 
 gulp.task('watch', function() {
     livereload.listen();
-    gulp.watch(['index.html', "js/*.js"]).on('change', livereload.changed);
+    gulp.watch(['index.html', "js/*.js", "css/*.css"]).on('change', livereload.changed);
 });
