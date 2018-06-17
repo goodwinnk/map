8f092cee8febc18bec9b7567fa09ffd583d93d0e
Nikolay Krasko <goodwinnk@gmail.com>
2018-06-17 13:34:25 +0300

    Always pain in 100% and scale afterwards

--- web/js/main.js
+++ web/js/main.js
@@ -1,33 +1,34 @@
+var SIZE = 20;
+var RADIUS = SIZE / 2 * Math.sqrt(2);
+var MILISECONDS_IN_YEAR = 31536000000;
+var HEXAGON_POINTS = hexagon(RADIUS);
+
 var width = window.innerWidth,
     height = window.innerHeight;
+
+var scale = 1;
+
+var zoom = d3.zoom().on("zoom", zoomed);
 
 var svg = d3.select("#map")
     .append("svg")
     .attr("class", "view")
     .attr("width", width)
     .attr("height", height)
-    .call(d3.zoom().scaleExtent([1, 8]).on("zoom", zoom))
-    .append("g");
+    .call(zoom);
+
+var mainG = svg.append("g");
 
 function addIssues(issues) {
     var issuesWithSubsystems = splitToSubsystems(issues);
 
-    var root = d3.hierarchy(issuesWithSubsystems)
-        .sum(function (d) {
-            return 20;
-        });
+    var root = d3.hierarchy(issuesWithSubsystems).count();
 
     var bubble = d3.pack()
         .size([width, height])
         .padding(function (d) {
             if (d === root) {
-                var firstGroupNode = root.children[0];
-                if (firstGroupNode) {
-                    var firstLeaf = firstGroupNode.children[0];
-                    if (firstLeaf) {
-                        return 20 * 2;
-                    }
-                }
+                return RADIUS;
             }
 
             return 0;
@@ -35,7 +36,24 @@ function addIssues(issues) {
 
     bubble(root);
 
-    var groupNode = svg.selectAll(".group")
+    var leafR = RADIUS;
+    var firstGroupNode = root.children[0];
+    if (firstGroupNode) {
+        var firstLeaf = firstGroupNode.children[0];
+        if (firstLeaf) {
+            leafR = firstLeaf.r;
+        }
+    }
+
+    scale = leafR / RADIUS;
+
+    root.each(function (d) {
+        d.x /= scale;
+        d.y /= scale;
+        d.r /= scale;
+    });
+
+    var groupNode = mainG.selectAll(".group")
         .data(root.descendants().filter(function (d) {
             return d !== root && d.children;
         }))
@@ -48,9 +66,9 @@ function addIssues(issues) {
     groupNode
         .append("circle")
         .attr("r", function (d) {
-            var firstLeaf = d.children[0];
-            var delta = firstLeaf ? firstLeaf.r / 2 : 0;
-            return d.r + 5;
+            // var firstLeaf = d.children[0];
+            // var delta = firstLeaf ? firstLeaf.r / 2 : 0;
+            return d.r;
         });
 
     groupNode
@@ -59,24 +77,11 @@ function addIssues(issues) {
             return "" + d.data.groups;
         });
 
-    // groupNode
-    //     .append("text")
-    //     .text(function (d) {
-    //         return "" + d.groups;
-    //     })
-    //     .attr('font-size', function (d) {
-    //         return "0.1em";
-    //     })
-    //     .style("text-anchor", "middle")
-    //     .attr("y", function (d) {
-    //         -d.y;
-    //     });
-
-    var node = svg.selectAll(".issue")
+    var node = mainG.selectAll(".issue")
         .data(root.leaves().sort(function (a, b) {
             var dy = a.y - b.y;
-            if (dy !== 0) {
-                return dy
+            if (dy > 0.001) {
+                return dy;
             }
             return -(a.x - b.x);
         }))
@@ -98,46 +103,46 @@ function addIssues(issues) {
         });
 
     node.append("polygon")
-        .attr("points", function (d) {
-            return hexagon(d.r);
-        })
-        .style("fill", function (d) {
+        .attr("points", HEXAGON_POINTS)
+        .attr("class", function (d) {
             var priority = decodePriority(d.data.p);
             if (priority === "Minor") {
-                return "lightgreen"
+                return "minor_priority";
             } else if (priority === "Normal") {
-                return "forestgreen";
+                return "normal_priority";
             } else if (priority === "Major") {
-                return "orange";
+                return "major_priority";
             } else if (priority === "Critical") {
-                return "deeppink"
+                return "critical_priority";
             }
 
-            return "gray"
-        })
-        .style("stroke", "lightgray")
-        .style("stroke-width", function (d) {
-            return d.r / 20;
+            return "unknown_priority";
         })
     ;
-    // // node.append("image")
-    // //     .attr("xlink:href", "../img/trees.png")
-    // //     .attr("x", function (d) {
-    // //         return -d.r
-    // //     })
-    // //     .attr("y", function (d) {
-    // //         return -d.r;
-    // //     })
-    // //     .attr("width", function (d) {
-    // //         return d.r * 2;
-    // //     })
-    // //     .attr("height", function (d) {
-    // //         return d.r * 2;
-    // //     });
-    //
-    var MILISECONDS_IN_YEAR = 31536000000;
-    var today = Date.now();
 
+    // node
+    //     .append("image")
+    //     .attr("xlink:href", function (d) {
+    //         var priority = decodePriority(d.data.p);
+    //         if (priority === "Minor") {
+    //             return "../img/bushes.png"
+    //         } else if (priority === "Normal") {
+    //             return "../img/trees.png";
+    //         } else if (priority === "Major") {
+    //             return "../img/dunes.png";
+    //         } else if (priority === "Critical") {
+    //             return "../img/vulcano.png"
+    //         }
+    //
+    //         return "../img/rocs.png"
+    //
+    //     })
+    //     .attr("x", -(SIZE / 2))
+    //     .attr("y", -(SIZE / 2))
+    //     .attr("width", SIZE)
+    //     .attr("height", SIZE);
+
+    var today = Date.now();
     for (var i = 1; i <= 5; i++) {
         var numberOfYears = i;
 
@@ -150,21 +155,32 @@ function addIssues(issues) {
                 return d.r / 6 * (6 - numberOfYears);
             })
             .attr("class", "year_circle")
-            .style("stroke-width", function (d) {
-                return 1 * d.r / 20;
-            })
         ;
     }
 
-    // node.filter(function (d) { return d.data.v > 0; })
-    //     .append('text')
-    //     .attr('font-family', 'FontAwesome')
-    //     .attr('font-size', function (d) {
-    //         return "0." + Math.floor(d.votes / 5) + "em";
+    node
+        .filter(function (d) {
+            return d.data.v > 0;
+        })
+        .append('text')
+        .attr('font-family', 'FontAwesome')
+        .attr('font-size', function (d) {
+            return "" + Math.ceil(Math.max(d.data.v, 50) / 50 * d.r) + "px";
+        })
+        // .attr('x', function (d) { return -3; })
+        // .attr('y', function (d) { return 3; })
+        .text('\uf087');
+
+    // groupNode
+    //     .append("text")
+    //     .text(function (d) {
+    //         return "" + d.groups;
     //     })
-    //     .attr('x', function (d) { return -3; })
-    //     .attr('y', function (d) { return 3; })
-    //     .text(function(d) { return '\uf087'; });
+    //     .style("text-anchor", "middle")
+    // ;
+
+    var t = d3.zoomIdentity.scale(scale);
+    zoom.transform(svg, t);
 }
 
 function splitToSubsystems(issues) {
@@ -192,9 +208,9 @@ function splitToSubsystems(issues) {
     };
 }
 
-function zoom() {
+function zoomed() {
     var event = d3.event;
-    svg.attr("transform", event.transform);
+    mainG.attr("transform", event.transform);
 }
 
 function hexagon(r) {
