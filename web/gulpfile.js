9df94e457e752c36fd9065916f3edd27a4bd9949
Nikolay Krasko <goodwinnk@gmail.com>
2018-07-23 03:11:15 +0300

    Dist gulp task

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,10 +1,28 @@
 var gulp = require('gulp');
 var livereload = require('gulp-livereload');
 
-gulp.task('default', function() {
+gulp.task('default', function () {
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     livereload.listen();
     gulp.watch(['index.html', "js/*.js", "css/*.css"]).on('change', livereload.changed);
+});
+
+gulp.task('dist', function () {
+    gulp.src([
+        './index.html',
+        './js/*.js',
+        './img/*.png',
+        './css/*.css',
+        './data/2018.07.23/*.json',
+        './bower_components/jquery/dist/jquery.min.js',
+        './bower_components/bootstrap/dist/js/bootstrap.min.js',
+        './bower_components/d3/d3.min.js',
+        './bower_components/store-js/dist/store.legacy.min.js',
+        './bower_components/bootstrap/dist/css/bootstrap.min.css'
+    ], {base: "."})
+        .pipe(gulp.dest(function (file) {
+            return './web/' + file.base;
+        }));
 });
